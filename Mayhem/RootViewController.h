e06ba40158ea964342fb69d457a1526f4ac49012
Felles <post@spicyturtle.com>
2011-04-03 16:17:23 +0200

    Initial Commit

---
+++ Mayhem/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Mayhem
+//
+//  Created by Tor Kreutzer on 4/3/11.
+//  Copyright UiT 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
