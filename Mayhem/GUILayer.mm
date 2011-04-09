aa20122b3d2a966974cddec4f34d0ae1d2a660e0
Felles <post@spicyturtle.com>
2011-04-09 20:00:43 +0200

    Switched to old buttons.

--- Mayhem/GUILayer.mm
+++ Mayhem/GUILayer.mm
@@ -21,7 +21,7 @@
     if (self) {
         // Get winSize
         CGSize winSize = GET_WINSIZE();
-        
+        /*
         // Create button sprites
         // Turn Left
         MenuButton *leftButton = [MenuButton buttonFromFile:@"ButtonLeft.png" Target:player selector:@selector(turnLeft)];
@@ -38,10 +38,47 @@
         fireButton.position = ccp(winSize.width - 60.0, 60.0);
         [self addChild:fireButton];
         
+        
         // Accelerate
         MenuButton *accelerateButton = [MenuButton buttonFromFile:@"ButtonAccelerate.png" Target:player selector:@selector(accelerate)];
         accelerateButton.position = ccp(winSize.width - 120.0, 60.0);
         [self addChild:accelerateButton];
+
+        */
+        
+        // Create the four controller buttons
+        // Turn left
+        CCMenuItem *turnLeftButton;
+        turnLeftButton = [CCMenuItemImage itemFromNormalImage:@"ButtonLeft.png" selectedImage:@"ButtonLeftSel.png" target:player selector:@selector(turnLeft)];
+        turnLeftButton.position = ccp(60,60);
+
+        // Turn right
+        CCMenuItem *turnRightButton;
+        turnRightButton = [CCMenuItemImage itemFromNormalImage:@"ButtonRight.png" selectedImage:@"ButtonRightSel.png" target:player selector:@selector(turnRight)];
+        turnRightButton.position = ccp(120,60);
+        
+        // Fire
+        CCMenuItem *fireButton;
+        fireButton = [CCMenuItemImage itemFromNormalImage:@"ButtonFire.png" selectedImage:@"ButtonFireSel.png" block: ^(id sender)
+                      {
+                          Weapon *bullet = [player fire];
+                          
+                          [player.parent addChild:bullet];
+                    
+                      }];
+        fireButton.position = ccp(winSize.width - 60 ,60);  
+        
+        // Thrust
+        CCMenuItem *accelerateButton;
+        accelerateButton = [CCMenuItemImage itemFromNormalImage:@"ButtonAccelerate.png" selectedImage:@"ButtonAccelerateSel.png" target:player selector:@selector(accelerate)];
+        accelerateButton.position = ccp(winSize.width - 120 ,60);
+        
+        // Create Menu with all items and add to layer
+        CCMenu *gui;
+        gui = [CCMenu menuWithItems:turnLeftButton, turnRightButton, accelerateButton, fireButton , nil];
+        gui.position = CGPointZero;
+        
+        [self addChild:gui];
     }
     return self;
 }
